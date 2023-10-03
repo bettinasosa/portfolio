a0fddbd29568beed4ee0bbbda52a70a9238a6f14
Bettina Sosa <bettina@catapultlabs.xyz>
2023-10-03 14:42:35 +0100

    added a loading landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
 import netlify from '@astrojs/netlify/functions';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   polyfills: true,
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), react()],
   output: 'server',
   adapter: netlify()
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,10 +10,10 @@ module.exports = {
       current: 'currentColor',
       transparent: 'transparent',
       white: '#ffffff',
-      primary: colors.stone,
+      primary: colors.stone
     },
     fontFamily: {
-      sans: ['Inter', 'sans-serif'],
+      sans: ['Inter', 'sans-serif']
     },
     fontSize: {
       xs: ['0.75rem', '1rem'],
@@ -25,7 +25,79 @@ module.exports = {
       '3xl': ['1.875rem', '2.375rem'],
       '4xl': ['2.25rem', '2.75rem'],
       '5xl': ['3rem', '3.5rem'],
-      '6xl': ['3.75rem', '4.25rem'],
+      '6xl': ['3.75rem', '4.25rem']
     },
-  },
+    extend: {
+      backgroundImage: {
+        'gradient-radial':
+          'radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))'
+      },
+      animation: {
+        'fade-in': 'fade-in 3s ease-in-out forwards',
+        title: 'title 3s ease-out forwards',
+        'fade-left': 'fade-left 3s ease-in-out forwards',
+        'fade-right': 'fade-right 3s ease-in-out forwards'
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': {
+            opacity: '0%'
+          },
+          '75%': {
+            opacity: '0%'
+          },
+          '100%': {
+            opacity: '100%'
+          }
+        },
+        'fade-left': {
+          '0%': {
+            transform: 'translateX(100%)',
+            opacity: '0%'
+          },
+
+          '30%': {
+            transform: 'translateX(0%)',
+            opacity: '100%'
+          },
+          '100%': {
+            opacity: '0%'
+          }
+        },
+        'fade-right': {
+          '0%': {
+            transform: 'translateX(-100%)',
+            opacity: '0%'
+          },
+
+          '30%': {
+            transform: 'translateX(0%)',
+            opacity: '100%'
+          },
+          '100%': {
+            opacity: '0%'
+          }
+        },
+        title: {
+          '0%': {
+            'line-height': '0%',
+            'letter-spacing': '0.25em',
+            opacity: '0'
+          },
+          '25%': {
+            'line-height': '0%',
+            opacity: '0%'
+          },
+          '80%': {
+            opacity: '100%'
+          },
+
+          '100%': {
+            'line-height': '100%',
+            opacity: '100%'
+          }
+        }
+      }
+    }
+  }
 };
