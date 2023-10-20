de4cf8889218dc12a45036b2b0a3435d02caf39b
Bettina Sosa <bettina@catapultlabs.xyz>
2023-10-20 11:04:56 +0100

    Fixed mobile view added google analytics back and passed some md files to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,17 @@ import compress from 'astro-compress';
 import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
   polyfills: true,
   integrations: [tailwind(), compress(), react(), mdx()],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
+  markdown: {
+    // Applied to .md and .mdx files
+    remarkPlugins: [remarkToc]
+  }
 });
