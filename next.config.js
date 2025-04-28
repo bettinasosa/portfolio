11041cb9fabc421b6cabea25fda0c36a441c72b9
Bettina Sosa <bettinasosa@partisiablockchain.com>
2025-04-28 21:41:16 +0200

    added blog api to page

--- next.config.js
+++ next.config.js
@@ -5,8 +5,12 @@ const nextConfig = {
       'i.scdn.co',
       'image-cdn-ak.spotifycdn.com',
       'blend-playlist-covers.spotifycdn.com',
-      'image-cdn-fa.spotifycdn.com'
-    ]
+      'image-cdn-fa.spotifycdn.com',
+      'miro.medium.com',
+      'cdn-images-1.medium.com'
+    ],
+    unoptimized: false
   }
 };
+
 export default nextConfig;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,22 @@
-export default function Page() {
+import Layout from '@/components/layout';
+import { getBlogPosts } from './actions';
+import { BlogList } from './components/BlogList';
+import { BlogHero } from './components/BlogHero';
+import { Suspense } from 'react';
+
+export default async function Page() {
+  const posts = await getBlogPosts();
+
   return (
-    <div className="flex h-full min-h-screen w-full items-center justify-center">
-      <h1 className="text-[6vh] font-semibold">Coming soon</h1>
-    </div>
+    <Layout title={'Blog'}>
+      <div className="container mx-auto px-4 py-16">
+        <BlogHero />
+        <Suspense
+          fallback={<div className="py-12 text-center">Loading posts...</div>}
+        >
+          <BlogList posts={posts} />
+        </Suspense>
+      </div>
+    </Layout>
   );
 }
